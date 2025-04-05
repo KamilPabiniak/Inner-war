36602201c954b09d431cda3c1918cc1f15aee400
CzarnyTheStudent <kpabiniak@edu.cdv.pl>
2025-04-05 14:55:12 +0200

    Krzychu, morda mam dla ciebie zapisik he he

--- Assets/Scripts/Managers/AudioMixerManager.cs
+++ Assets/Scripts/Managers/AudioMixerManager.cs
@@ -1,20 +1,73 @@
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 namespace Managers
 {
     public class AudioMixerManager : MonoBehaviour
     {
         [SerializeField] private AudioMixer audioMixer;
+        [SerializeField] private Slider sfxSlider;
+        [SerializeField] private Slider musicSlider;
+
+        private const string SfxKey = "SFXVolume";
+        private const string MusicKey = "MusicVolume";
+        
+        private float _currentSfxVolume = 0.5f;
+        private float _currentMusicVolume = 0.5f;
+
+        private void Start()
+        {
+            LoadVolumeSettings();
+            InitializeSliders();
+        }
+        
+        private void InitializeSliders()
+        {
+            if (sfxSlider != null)
+            {
+                sfxSlider.value = PlayerPrefs.GetFloat(SfxKey, 0.5f);
+                sfxSlider.onValueChanged.AddListener(SetSoundFXVolume);
+            }
+
+            if (musicSlider != null)
+            {
+                musicSlider.value = PlayerPrefs.GetFloat(MusicKey, 0.5f);
+                musicSlider.onValueChanged.AddListener(SetMusicVolume);
+            }
+        }
 
         public void SetSoundFXVolume(float level)
         {
-            audioMixer.SetFloat("volumeSFX", Mathf.Log10(level) * 20f);
+            _currentSfxVolume = level;
+            float dbVolume = level > 0.001f ? Mathf.Log10(level) * 20f : -80f;
+            audioMixer.SetFloat("volumeSFX", dbVolume);
+            SaveVolumeSettings();
         }
     
         public void SetMusicVolume(float level)
         {
-            audioMixer.SetFloat("volumeMusic", Mathf.Log10(level) * 20f);
+            _currentMusicVolume = level;
+            float dbVolume = level > 0.001f ? Mathf.Log10(level) * 20f : -80f;
+            audioMixer.SetFloat("volumeMusic", dbVolume);
+            SaveVolumeSettings();
+        }
+    
+        private void SaveVolumeSettings()
+        {
+            PlayerPrefs.SetFloat(SfxKey, _currentSfxVolume);
+            PlayerPrefs.SetFloat(MusicKey, _currentMusicVolume);
+            PlayerPrefs.Save();
+        }
+    
+        private void LoadVolumeSettings()
+        {
+            _currentSfxVolume = PlayerPrefs.GetFloat(SfxKey, _currentSfxVolume);
+            _currentMusicVolume = PlayerPrefs.GetFloat(MusicKey, _currentMusicVolume);
+            float sfxDb = _currentSfxVolume > 0.001f ? Mathf.Log10(_currentSfxVolume) * 20f : -80f;
+            float musicDb = _currentMusicVolume > 0.001f ? Mathf.Log10(_currentMusicVolume) * 20f : -80f;
+            audioMixer.SetFloat("volumeSFX", sfxDb);
+            audioMixer.SetFloat("volumeMusic", musicDb);
         }
     }
 }
